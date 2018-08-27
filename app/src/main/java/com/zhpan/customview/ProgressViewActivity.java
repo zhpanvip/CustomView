2bb6098c25f47e180312627a346049f88d4fe706
zhpan <zhangp@oitsme.com>
2018-08-27 16:26:43 +0800

    Complete CircleProgressView

--- app/src/main/java/com/zhpan/customview/ProgressViewActivity.java
+++ app/src/main/java/com/zhpan/customview/ProgressViewActivity.java
@@ -4,22 +4,21 @@ import android.os.Handler;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
-
-import com.zhpan.custom_circle_progress.CircleProgress;
+import com.zhpan.custom_circle_progress.CircleProgressView;
 
 public class ProgressViewActivity extends AppCompatActivity {
 
-    private CircleProgress mCircleProgress;
+    private CircleProgressView mCircleProgress;
     private int progress;
     private static Handler mHandler = new Handler();
     private boolean isRunning;
     private Runnable mRunnable = new Runnable() {
         @Override
         public void run() {
-            progress++;
             mCircleProgress.setProgress(progress);
             if (progress < 100) {
-                mHandler.postDelayed(this, 30);
+                progress++;
+                mHandler.postDelayed(this, 60);
             } else {
                 isRunning = false;
                 mHandler.removeCallbacks(this);
@@ -30,10 +29,10 @@ public class ProgressViewActivity extends AppCompatActivity {
     private Runnable mRunnableReset = new Runnable() {
         @Override
         public void run() {
-            progress--;
             mCircleProgress.setProgress(progress);
             if (progress > 0) {
-                mHandler.postDelayed(this, 3);
+                progress--;
+                mHandler.postDelayed(this, 1);
             } else {
                 isRunning = false;
                 mHandler.removeCallbacks(this);
@@ -46,7 +45,8 @@ public class ProgressViewActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_progress_view);
         mCircleProgress = findViewById(R.id.cp_view);
-
+        mCircleProgress.setShowText(true);
+        mCircleProgress.setRingWidthPercent(7);
     }
 
     public void onClick(View view) {
@@ -54,20 +54,18 @@ public class ProgressViewActivity extends AppCompatActivity {
             case R.id.btn_line_chart:
                 start();
                 break;
-
             case R.id.btn_pie_chart:
                 stop();
                 break;
             case R.id.btn_lock_view:
                 reset();
-
                 break;
         }
     }
 
     private void reset() {
         stop();
-        mHandler.postDelayed(mRunnableReset,3);
+        mHandler.postDelayed(mRunnableReset,1);
     }
 
     private void start() {
