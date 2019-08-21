4d30ce48a03e939162ed4106a6575a3893f17178
张攀 <zhangp@oitsme.com>
2019-08-22 02:33:29 +0800

    Fix Error

--- app/src/main/java/com/zhpan/customview/LineChartActivity.java
+++ app/src/main/java/com/zhpan/customview/LineChartActivity.java
@@ -7,6 +7,7 @@ import android.util.Log;
 import android.view.View;
 import android.view.animation.DecelerateInterpolator;
 
+
 import com.zhpan.custom_line_chart.LineChartView;
 
 import java.util.ArrayList;
